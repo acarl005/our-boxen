e2c154680e7d1bfbc548391e6bc00e1362a652f2
Andrew Carlson <acarl005@g.ucla.edu>
2015-11-03 14:33:08 -0800

    move classes out of node definition

--- manifests/site.pp
+++ manifests/site.pp
@@ -51,6 +51,11 @@ Service {
 
 Homebrew::Formula <| |> -> Package <| |>
 
+# Set the global default node (auto-installs it if it can)
+class { 'nodejs::global':
+  version => '4.2.1'
+}
+
 node default {
   # core modules, needed for most things
   include dnsmasq
@@ -61,11 +66,6 @@ node default {
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
-  }
-
-  # Set the global default node (auto-installs it if it can)
-  class { 'nodejs::global':
-    version => '4.2.1'
   }
 
   # ensure a npm module is installed for a certain node version
@@ -83,7 +83,6 @@ node default {
     node_version => '*',
   }
 
-  # install a node version
 
   # Installing nodenv plugin
   nodejs::nodenv::plugin { 'nodenv-vars':
@@ -134,63 +133,63 @@ node default {
   # install the monokai theme
   atom::theme { 'monokai': }
 
+}
 
 
-  class config::sublime {
+class config::sublime {
 
-    define addpkg {
-      $packagedir = "/Library/Application Support/Sublime Text 2/Packages/"
-      $pkgarray = split($name, '[/]')
-      $pkgname = $pkgarray[1]
+  define addpkg {
+    $packagedir = "/Library/Application Support/Sublime Text 2/Packages/"
+    $pkgarray = split($name, '[/]')
+    $pkgname = $pkgarray[1]
 
-      exec { "git clone https://github.com/${name}.git":
-        cwd      => "/Users/${::luser}${packagedir}",
-        provider => 'shell',
-        creates  => "/Users/${::luser}${packagedir}${pkgname}",
-        path     => "${boxen::config::homebrewdir}/bin",
-        require  => [Package['SublimeText2'], Class['git']],
-      }
+    exec { "git clone https://github.com/${name}.git":
+      cwd      => "/Users/${::luser}${packagedir}",
+      provider => 'shell',
+      creates  => "/Users/${::luser}${packagedir}${pkgname}",
+      path     => "${boxen::config::homebrewdir}/bin",
+      require  => [Package['SublimeText2'], Class['git']],
     }
-
-    $base = "/Users/${::luser}/Library/Application Support"
-    $structure = [ "${base}/Sublime Text 2", "${base}/Sublime Text 2/Packages" ]
-
-    file { $structure:
-      ensure  => 'directory',
-      owner   => "${::luser}",
-      mode    => '0755',
-    }->
-
-    file { "${boxen::config::bindir}/subl":
-      ensure  => link,
-      target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
-      mode    => '0755',
-      require => Package['SublimeText2'],
-    }->
-
-    file { "${base}/Sublime Text 2/Packages/User/Default (OSX).sublime-keymap":
-      content  => '[{ "keys": ["super+ctrl+r"], "command": "reveal_in_side_bar"}]',
-    }->
-
-    file { "${base}/Sublime Text 2/Packages/User/Preferences.sublime-settings":
-        content  => '
-{
-  "trim_trailing_white_space_on_save": true,
-  "tab_size": 2,
-  "translate_tabs_to_spaces": true,
-  "save_on_focus_lost": true
-}'
-    }
-
-    addpkg { [
-      "jisaacks/GitGutter",
-      "revolunet/sublimetext-markdown-preview",
-      "SublimeColors/Solarized",
-      "wbond/sublime_package_control",
-      "eklein/sublime-text-puppet",
-      "sergeche/emmet-sublime"
-      ]:
-    }
-
   }
+
+  $base = "/Users/${::luser}/Library/Application Support"
+  $structure = [ "${base}/Sublime Text 2", "${base}/Sublime Text 2/Packages" ]
+
+  file { $structure:
+    ensure  => 'directory',
+    owner   => "${::luser}",
+    mode    => '0755',
+  }->
+
+  file { "${boxen::config::bindir}/subl":
+    ensure  => link,
+    target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
+    mode    => '0755',
+    require => Package['SublimeText2'],
+  }->
+
+  file { "${base}/Sublime Text 2/Packages/User/Default (OSX).sublime-keymap":
+    content  => '[{ "keys": ["super+ctrl+r"], "command": "reveal_in_side_bar"}]',
+  }->
+
+  file { "${base}/Sublime Text 2/Packages/User/Preferences.sublime-settings":
+      content  => '
+{
+"trim_trailing_white_space_on_save": true,
+"tab_size": 2,
+"translate_tabs_to_spaces": true,
+"save_on_focus_lost": true
+}'
+  }
+
+  addpkg { [
+    "jisaacks/GitGutter",
+    "revolunet/sublimetext-markdown-preview",
+    "SublimeColors/Solarized",
+    "wbond/sublime_package_control",
+    "eklein/sublime-text-puppet",
+    "sergeche/emmet-sublime"
+    ]:
+  }
+
 }
